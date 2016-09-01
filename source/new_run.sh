50cfe2866817429179a5e9784e0e85ba620ab2d4
fiht <fiht@qq.com>
2016-09-01 22:29:06 +0800

    正式能运行的版本

--- source/new_run.sh
+++ source/new_run.sh
@@ -2,10 +2,12 @@ while :
 do
 	target_name=$(find . -size 0c )
 	echo $target_name
-	for i in $(cat $1)
+	for i in $target_name
 	do
-		python get_subDomain.py -u $i -c -w
+		python get_subDomain.py -u $(echo $i| awk -F "_" '{print $1}') -c -w
 		sleep 5
+                echo $data"加入了一个文件"$$i
 	done
+echo "waiting..."
 sleep 20
 done

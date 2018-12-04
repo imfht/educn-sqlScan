f94d752d3e8cb0c8100c119d0b1dafdb28d2e4f4
ubuntu <ubuntu@localhost.localdomain>
2018-12-04 22:12:50 +0800

    updated.

--- source/run.sh
+++
@@ -1,5 +0,0 @@
-for i in $(cat $1)
-do
-	python get_subDomain.py -u $i -c -w
-	sleep 5
-done
--- 站点序列/run.sh
+++
@@ -1,6 +0,0 @@
-for i in $(ls $1):
-do
- newfile_name=$(echo $i| awk -F "_" '{print $1}')
- mv $i $newfile_name
-# echo $i---$newfile_name
-done 
